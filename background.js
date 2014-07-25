858a868db03b88883cb7e78ef62ac4ce395b459d
Munkhbaatar <gmunkhbaatarmn@gmail.com>
2014-07-25 13:35:12 +0800

    cdn change fix and more specific types

--- background.js
+++ background.js
@@ -1,11 +1,12 @@
-/*
- * Load CDN stored mathjax from Facebook.com
- */
+/* Load CDN stored mathjax from Facebook.com */
 chrome.webRequest.onHeadersReceived.addListener(function(details) {
   var i = 0
   while (i < details.responseHeaders.length) {
     if (details.responseHeaders[i].name.toUpperCase() === "CONTENT-SECURITY-POLICY") {
-      details.responseHeaders[i].value = details.responseHeaders[i].value.replace("script-src", "script-src https://c328740.ssl.cf1.rackcdn.com")
+      details.responseHeaders[i].value = details.responseHeaders[i].value.replace(
+        "script-src",
+        "script-src http://cdn.mathjax.org https://cdn.mathjax.org https://*.rackcdn.com"
+      )
     }
     i++
   }
@@ -14,5 +15,5 @@ chrome.webRequest.onHeadersReceived.addListener(function(details) {
   }
 }, {
   urls: ["*://*.facebook.com/*"],
-  types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+  types: ["main_frame", "sub_frame", "xmlhttprequest", "other"]
 }, ["blocking", "responseHeaders"])
